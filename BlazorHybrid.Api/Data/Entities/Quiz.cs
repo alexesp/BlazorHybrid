980d9737ea32605ade89ed4bd4bb72389e411834
alexesp <alexizaes@hotmail.com>
2025-05-08 00:38:18 +0200

    Crear Entities

---
+++ BlazorHybrid.Api/Data/Entities/Quiz.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorHybrid.Api.Data.Entities
+{
+    public class Quiz
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public int TotalQuestions { get; set; }
+        public int  TimeInMinutes { get; set; }
+        public bool IsActive { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+
+        public ICollection<Question> Questions { get; set; }
+    }
+
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public Guid QuizId { get; set; }
+        [ForeignKey(nameof(QuizId))] 
+        public Quiz Quiz { get; set; }
+    }
+
+    public class Option
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
---
+++ BlazorHybrid.Api/Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BlazorHybrid.Shared;
+
+namespace BlazorHybrid.Api.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [MaxLength(150)]
+        public string Email { get; set; }
+        [MaxLength(250)]
+        public string PasswordHash { get; set; }
+        [Length(10, 15)]
+        public string Phone {  get; set; }
+        [MaxLength(15)]
+        public string Role { get; set; } = nameof(UserRole.User);
+    }
+}
